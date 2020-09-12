746b982b31ece43f1ca3b4363f7a4965e2320dda
dmlrnl1234 <71161257+dmlrnl1234@users.noreply.github.com>
2020-09-12 11:19:54 +0900

    Create index.py

---
+++ index.py
@@ -0,0 +1,26 @@
+live_in_jeju = True
+if live_in_jeju:
+    print('제주에 살아요')
+name = '은규'
+if name == '은규':
+    print('제이름은 은규에요')
+else:
+    print('이름이 은규가 아니에요')
+age = 15
+if age > 13:
+    print('전 13살보다 위에요')
+else:
+    print('전 13살보다 밑이에요')
+odd_number = [1, 2, 3, 5]
+for num in odd_number:
+    print(odd_number)
+number = [1, 2, 3, 5, 6, 9]
+for num in number:
+   if (num % 2) == 1:
+    print(num)
+ages = [12, 14, 17, 22, 26]
+for i in ages:
+    if i > 20:
+        print('여기서 멈출게요')
+        # break
+
